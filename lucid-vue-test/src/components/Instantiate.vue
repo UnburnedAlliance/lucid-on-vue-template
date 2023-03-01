859257cabc9557550d4355b64b6139f812637918
UnburnedAlliance <linenotsecure@gmail.com>
2023-03-01 13:40:47 -0600

    Fuck this fucking bullshit fucking fucker. Berry make some goddamn better documentation for Lucid goddamn fuck

--- lucid-vue-test/src/components/Instantiate.vue
+++ lucid-vue-test/src/components/Instantiate.vue
@@ -5,39 +5,89 @@ const lucid = await Lucid.new(
  await new Blockfrost("https://cardano-mainnet.blockfrost.io/api/v0", "mainnetZLfKC8wgc5vNbe31vGt5sK6u8lQxoNxm"), "Mainnet",
 );
 
-const alreadyEnabled = await window.cardano.isEnabled();
+
+
+let alreadyEnabled = await window.cardano.nami.isEnabled();
+console.log("WTF")
+console.log(await window.cardano.nami.isEnabled())
 //const wtf = await window.cardano.nami.enable();
+if(alreadyEnabled) {
+  console.log("Detected whether enabled: ".concat(alreadyEnabled));
+} else {
+  console.log("not yet enabled")
+  console.log(alreadyEnabled)
+}
 
 export default {
   data() {
     return {
-      lucidAPI: lucid,
-      connectmsg: "Connect",
-      walletname: "UNK",
-      walletbal: "UNK",
+      lucidAPI: undefined,
+      connectmsg: undefined,
+      walletname: undefined,
+      walletbal: undefined,
+      networkid: undefined,
+      firstaddy: undefined,
+      apiv: undefined,
     }
   },
   methods: {
     connectWallet: function () {
-      console.log('connectWallet function')
-      const api = window.cardano.nami.enable();
-      this.lucidAPI.selectWallet(api);
-      this.walletname = this.lucidAPI.network;
-      console.log(this.lucidAPI);  
+      async function fuckthisbullshit() {
+        console.log('connectWallet function');
+        const api = window.cardano.nami.enable();
+        lucid.selectWallet(api);
+        this.lucidAPI = api;
+        console.log("inside async function")
+        this.walletname = (await this.lucidAPI).getChangeAddress();
+        this.networkid = (await this.lucidAPI).getNetworkId();
+        this.walletname = window.cardano.nami.name;
+        this.walletbal = (await this.lucidAPI).getBalance();
+        this.connectmsg = "Connected to ".concat(this.walletname);
+        this.firstaddy = (await this.lucidAPI).getChangeAddress();
+        this.apiv = window.cardano.nami.apiVersion;
+        console.log("connectWallet() activating uiSwitch(true)")
+        this.uiSwitch(true);
+      }
+      fuckthisbullshit();
       console.log(window.cardano.nami.name) 
+    },
+    uiSwitch: function(connected) {
+      console.log("in uiswitch")
+      if(connected == true) {
+        console.log("connected == true??????????")
+        let el = document.getElementById("connectioninfobox");
+        el.style.display = "inherit";
+        this.connectmsg = "Connected ".concat(this.walletname)
+      } else {
+        console.log("switching ui elements off")
+        let el = document.getElementById("connectioninfobox");
+        el.style.display = "none";
+        this.connectmsg = "Disconnected";
+      }
+    },
+    checkBrowser: function() {
+      console.log("checkBrowser function")
+      async function isConnected() {
+        let walletOn = await window.cardano.nami.isEnabled();
+        console.log("WTF!!!! ", walletOn);
+        
+        return false;
+      }
+      console.log('connected? '.concat(isConnected()));
+      if(isConnected()) {
+        console.log("window.cardano.nami.isEnabled == true???????")
+        let connectwhat = async () => { await window.cardano.nami.isEnabled()};
+        console.log(connectwhat);
+        this.uiSwitch(true);
+      } else {
+        console.log("Not connected, flipping uiSwitch(false)")
+        this.uiSwitch(false);
+      }
     }
   },
   mounted() {
     console.log('mounted section');
-    if(alreadyEnabled) {
-      console.log("cardano enabled.")
-      this.connectmsg = "Connected to ".concat(window.cardano.nami.name);
-      this.walletname = window.cardano.nami.name;
-      this.walletbal = "TBD";
-      console.log('getBalance....');
-      console.log(window.cardano.getUsedAddresses());
-    }
-    
+    this.uiSwitch(alreadyEnabled);
   },
   updated() {
     console.log('updated section');
@@ -65,12 +115,36 @@ export default {
 <button @click="connectWallet">
   {{ connectmsg }}
 </button>
-<h3>{{ walletname }}: {{ walletbal }}</h3>
+<div id="connectioninfobox">
+<ul>
+  <li>
+  Wallet Name: {{ walletname }}
+</li>
+<li>
+  Network: {{ networkid }}
+</li>
+<li>
+  Account Address: {{ firstaddy }}
+</li>
+<li>
+  API Version: {{  apiv }}
+</li>
+<li>
+  Balance: {{  walletbal }}
+</li>
+</ul>
+</div>
  </div>
 </template>
 
 <style scoped lang="scss">
 code {
   font-weight: bold;
+}
+
+ul {
+  list-style-type: "🔔";
+  padding-inline-start: 1em;
+  margin-top: 1em;
 }
 </style>
